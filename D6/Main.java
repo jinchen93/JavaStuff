5a696591b941f8018c99f6740fa0c38c9387802d
Jin Chen <jinchen93@gmail.com>
2018-06-17 11:35:59 -0700

    D6

---
+++ D6/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+    // Operators
+    // Similar to JavaScript
+
+    int result = 1 + 2;
+    result++;
+    result--;
+    result += 2;
+    result *= 2;
+    result = result % 3;
+
+    boolean myBool = true;
+
+    if (myBool == true)
+      System.out.println("My boolean is true");
+  }
+}
