2d23de1b41081223c9b2599babc45d0b32920d63
Jin Chen <jinchen93@gmail.com>
2018-06-17 11:27:32 -0700

    D4

---
+++ D4/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args){
+    char myChar = "A";
+    char myUnicodeChar = "\u00A9";
+
+    boolean myBool = true;
+    boolean myFalseBool = false;
+  }
+}
