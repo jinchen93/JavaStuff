5d9c554e1dccf204c0b4c7aedd47ee5b001cd56f
Jin Chen <jinchen93@gmail.com>
2018-06-16 19:13:10 -0700

    D2

---
+++ D2/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int minVal = -2_174_783_648;
+        int maxVal = 2_174_783_648;
+//        width 32
+
+        byte byteVal = -120;
+//        bytes use less space than ints
+//        width 8
+        byte asd = byteVal / 2;
+
+        short shortVal = 32767;
+//        shorts use more space than bytes but less than int
+//        width 16
+
+        long myVal = 100L;
+//        width 64
+    }
+}
