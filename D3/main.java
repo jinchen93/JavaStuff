d979a73cb343fb780832c34903a92b77cffc1722
Jin Chen <jinchen93@gmail.com>
2018-06-17 11:06:54 -0700

    D3

---
+++ D3/main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int myIntVal = 5;
+        float myFloatVal = 5.25f;
+        double myDoubleVal = 5.25;
+//        float and doubles have different
+//        levels of decimal precisions
+
+//        width of float = 32 (4 bytes)
+//        width of double = 64 (8 bytes)
+    }
+}
