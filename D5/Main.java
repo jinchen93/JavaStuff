b960271b68f8e076307659de1b3e0669c88a1549
Jin Chen <jinchen93@gmail.com>
2018-06-17 11:31:58 -0700

    D5

---
+++ D5/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+    // byte
+    // short
+    // int
+    // long
+    // float
+    // double
+    // char
+    // boolean
+
+    // 8 Java primitive types
+
+    String myStr = "This is a string";
+    myStr = myStr + ", and this is a concatted version";
+  }
+}
